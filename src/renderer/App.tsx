fed2a43601d8c2820a62b622a2e08cf6491ecd32
MobileDev <duytn@inspirelab.io>
2023-08-09 11:50:55 +0700

    [App] update UI

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,12 +2,14 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Verify from '../resource/screen/Verify';
+import Detail from '../resource/screen/Detail';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Verify />} />
+        <Route path="/Detail" element={<Detail />} />
       </Routes>
     </Router>
   );
---
+++ src/resource/screen/Detail/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Detail = () => {
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', flex: 1, padding: 16, rowGap: 12, backgroundColor: '#e0e0e0' }}>
+
+    </div>
+  )
+
+}
+
+export default Detail;
