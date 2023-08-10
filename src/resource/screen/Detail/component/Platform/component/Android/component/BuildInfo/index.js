74a038f03e090ba76cfc65a6e6ccbbbeaed6cce2
MobileDev <duytn@inspirelab.io>
2023-08-10 09:43:59 +0700

    [App] update UI

---
+++ src/resource/screen/Detail/component/Platform/component/Android/component/BuildInfo/index.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+
+import { IconButton } from '@mui/material';
+import { SettingsOutlined } from '@mui/icons-material';
+
+import { useAppStore } from '@app-utils';
+
+const BuildInfo = () => {
+  const appPath = useAppStore(state => state.appPath);
+
+  const [appInfo, setAppInfo] = useState({ appName: '', versionCode: '', versionName: '', bundleId: '' })
+
+  const [openModal, setOpenModal] = useState(false);
+
+  const _setModal = () => setOpenModal(!openModal);
+
+  const _getGradle = (gradle, strings) => {
+    const result = gradle.substr(gradle.indexOf(strings)).substr(0, gradle.substr(gradle.indexOf(strings)).indexOf('\n'))?.split(' ')?.[1];
+    return result?.replaceAll('"', '');
+  }
+
+  const _readData = async () => {
+    const gradle_result = await window.electron.ipcRenderer.invoke('fs.readFileSync', `${appPath}/android/app/build.gradle`);
+    const strings_result = await window.electron.ipcRenderer.invoke('fs.readFileSync', `${appPath}/android/app/src/main/res/values/strings.xml`);
+
+    const gradle = gradle_result || '';
+    const strings = strings_result || '';
+
+    const appName = strings?.replace?.(/(<([^>]+)>)/gi, "")?.split('\n')?.filter?.(el => !!el)?.[0]?.trim?.();
+    const versionCode = _getGradle(gradle, 'versionCode');
+    const versionName = _getGradle(gradle, 'versionName');
+    const bundleId = _getGradle(gradle, 'applicationId');
+
+    setAppInfo({ appName, versionCode, versionName, bundleId });
+  }
+
+  useEffect(() => { _readData(); }, []);
+
+  return (
+    <div>
+      <div style={{ display: 'flex', flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <h4>Info</h4>
+        <IconButton onClick={_setModal}><SettingsOutlined color='primary' /></IconButton>
+      </div>
+
+      <div style={{ display: 'flex', flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <div style={{ fontSize: 14 }}>Version Code: <span style={{ fontWeight: 'bold' }}>{appInfo.versionCode}</span></div>
+        <div style={{ fontSize: 14 }}>Version Name: <span style={{ fontWeight: 'bold' }}>{appInfo.versionName}</span></div>
+        <div style={{ fontSize: 14 }}>App Name: <span style={{ fontWeight: 'bold' }}>{appInfo.appName}</span></div>
+        <div style={{ fontSize: 14 }}>Bundle ID: <span style={{ fontWeight: 'bold' }}>{appInfo.bundleId}</span></div>
+      </div>
+
+    </div>
+  )
+
+}
+
+export default BuildInfo;
--- src/resource/screen/Detail/component/Platform/component/Android/index.js
+++ src/resource/screen/Detail/component/Platform/component/Android/index.js
@@ -2,10 +2,14 @@ import React, { useState, useMemo } from 'react';
 
 import { Box, Tabs, Tab } from '@mui/material';
 
+import BuildInfo from './component/BuildInfo';
+
 const Android = () => {
   const [tabIndex, setTabIndex] = useState(0)
 
   const _handleChange = (_, newValue) => setTabIndex(newValue);
+
+  const memoBuildInfo = useMemo(() => <BuildInfo />, []);
 
   return (
     <div style={{ paddingBottom: 8, borderRadius: 12 }}>
@@ -16,6 +20,8 @@ const Android = () => {
           <Tab label="PDF Document" />
         </Tabs>
       </Box>
+
+      {(tabIndex === 0) && memoBuildInfo}
 
     </div>
   )
