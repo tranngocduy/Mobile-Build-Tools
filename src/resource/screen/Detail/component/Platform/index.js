cd9095cbcd50fbbbb73995369528af7a570309f3
MobileDev <duytn@inspirelab.io>
2023-08-11 10:24:48 +0700

    [App] update UI

--- src/resource/screen/Detail/component/Platform/index.js
+++ src/resource/screen/Detail/component/Platform/index.js
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 
-import { Paper, FormControl, Select, MenuItem } from '@mui/material';
+import { Paper } from '@mui/material';
 
 import { useAppStore } from '@app-utils';
 
@@ -9,24 +9,10 @@ import Android from './component/Android';
 const Platform = () => {
   const appPlatform = useAppStore(state => state.appPlatform);
 
-  const _handleChange = event => {
-    useAppStore.getState()?.updatePlatform?.(event.target.value);
-  }
-
   const memoAndroid = useMemo(() => <Android />, [])
 
   return (
     <Paper style={{ paddingRight: 16, paddingLeft: 16, paddingBottom: 16, borderRadius: 12 }}>
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <h4>Platform: </h4>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-          <Select value={appPlatform} onChange={_handleChange}>
-            <MenuItem value='Android'>Android</MenuItem>
-            <MenuItem value='iOS'>iOS</MenuItem>
-          </Select>
-        </FormControl>
-      </div>
-
       {appPlatform === 'Android' && memoAndroid}
     </Paper>
   )
