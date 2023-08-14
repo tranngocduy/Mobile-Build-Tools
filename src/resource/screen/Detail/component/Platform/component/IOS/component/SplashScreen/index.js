d914bc2747b2beeaf16629e2955ac4c8098a092d
MobileDev <duytn@inspirelab.io>
2023-08-14 10:31:04 +0700

    [App] update UI

---
+++ src/resource/screen/Detail/component/Platform/component/IOS/component/SplashScreen/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { useAppStore } from '@app-utils';
+
+import ItemImage from '@app-component/ItemImage';
+
+const SplashScreen = () => {
+  const appPath = useAppStore(state => state.appPath);
+  const appProjectName = useAppStore(state => state.appProjectName);
+  const path = `${appPath}/ios/${appProjectName}/Images.xcassets/Image.imageset`;
+
+  const _image = { name: 'Image.png', size: { width: 1500, height: 2668 } };
+  const _image1x = { name: 'Image-1.png', size: { width: 1500, height: 2668 } };
+  const _image2x = { name: 'Image-2.png', size: { width: 1500, height: 2668 } };
+
+  return (
+    <div>
+      <h4>Splash Screen</h4>
+      <div style={{ display: 'flex', flex: 1, flexDirection: 'row', justifyContent: 'space-between', columnGap: 48 }}>
+        <div style={{ display: 'flex', flex: 1, flexDirection: 'column', rowGap: 8 }}>
+          <ItemImage item={_image} isMustMatch={true} path={path} />
+        </div>
+
+        <div style={{ display: 'flex', flex: 1, flexDirection: 'column', rowGap: 8 }}>
+          <ItemImage item={_image1x} isMustMatch={true} path={path} />
+        </div>
+
+        <div style={{ display: 'flex', flex: 1, flexDirection: 'column', rowGap: 8 }}>
+          <ItemImage item={_image2x} isMustMatch={true} path={path} />
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default SplashScreen;
--- src/resource/screen/Detail/component/Platform/component/IOS/index.js
+++ src/resource/screen/Detail/component/Platform/component/IOS/index.js
@@ -4,6 +4,7 @@ import { Box, Tabs, Tab } from '@mui/material';
 
 import BuildInfo from './component/BuildInfo';
 import IconApp from './component/IconApp';
+import SplashScreen from './component/SplashScreen';
 
 const IOS = () => {
   const [tabIndex, setTabIndex] = useState(0)
@@ -13,6 +14,8 @@ const IOS = () => {
   const memoBuildInfo = useMemo(() => <BuildInfo />, []);
 
   const memoIconApp = useMemo(() => <IconApp />, []);
+
+  const memoSplashScreen = useMemo(() => <SplashScreen />, []);
 
   return (
     <div style={{ paddingBottom: 8, borderRadius: 12 }}>
@@ -28,6 +31,8 @@ const IOS = () => {
       {(tabIndex === 0) && memoBuildInfo}
 
       {(tabIndex === 1) && memoIconApp}
+
+      {(tabIndex === 2) && memoSplashScreen}
 
     </div>
   )
