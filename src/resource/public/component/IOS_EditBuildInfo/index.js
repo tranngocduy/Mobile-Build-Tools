0273e4ca55d6bc9dc0da7cf2b08eec29903a9f38
MDev <duytn@Inspirelab.io>
2023-10-08 19:36:43 +0700

    [App] update UI

--- src/resource/public/component/IOS_EditBuildInfo/index.js
+++ src/resource/public/component/IOS_EditBuildInfo/index.js
@@ -2,20 +2,31 @@ import React, { useRef } from 'react';
 
 import { Modal, Button, Paper, TextField } from '@mui/material';
 
-const IOS_EditBuildInfo = ({ appInfo, appPath, readData, setModal }) => {
+const IOS_EditBuildInfo = ({ appInfo, path, readData, setModal }) => {
   const appName = useRef(appInfo?.appName || '');
 
-  const _onChangeAppName = e => (appName.current = e.nativeEvent.target.value);
+  const _onChangeAppName = e => (appName.current = e.nativeEvent.target.value.trim());
 
   const _apply = async () => {
+    const newName = !!appName?.current?.includes?.(' ') ? `"${appName.current}.app"` : `${appName.current}.app`;
+    const currentName = !!appInfo?.appName?.includes?.(' ') ? `"${appInfo?.appName}.app"` : `${appInfo?.appName}.app`;
+
+    const newProductName = !!appName?.current?.includes?.(' ') ? `"${appName.current}"` : `${appName.current}`;
+    const currentProductName = !!appInfo?.appName?.includes?.(' ') ? `"${appInfo?.appName}"` : `${appInfo?.appName}`;
+
     await window.electron.ipcRenderer.invoke(
       'exec.runScript',
-      `find ${appPath} -type f -print0 | xargs -0 perl -pi -w -e 's/${appInfo.appName}/${appName.current}/g;'`
+      `find ${path} -type f -print0 | xargs -0 perl -pi -w -e 's/\\* ${appInfo.appName}.app \\*/\\* ${appName.current}.app \\*/g;'`
     );
 
     await window.electron.ipcRenderer.invoke(
       'exec.runScript',
-      `find ${appPath} -type f -print0 | xargs -0 perl -pi -w -e 's/\\$\\(PRODUCT_NAME\\)/${appName.current}/g;'`
+      `find ${path} -type f -print0 | xargs -0 perl -pi -w -e 's/ path = ${currentName}/ path = ${newName}/g;'`
+    );
+
+    await window.electron.ipcRenderer.invoke(
+      'exec.runScript',
+      `find ${path} -type f -print0 | xargs -0 perl -pi -w -e 's/PRODUCT_NAME = ${currentProductName}/PRODUCT_NAME = ${newProductName}/g;'`
     );
 
     await readData();
