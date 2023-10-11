94bf169d6e68de913490a14d3ad42dfa46e77828
MobileDev <duytn@inspirelab.io>
2023-10-11 11:32:33 +0700

    [App] fix path

--- src/resource/public/component/ViewLocked/index.js
+++ src/resource/public/component/ViewLocked/index.js
@@ -16,11 +16,11 @@ const ViewLocked = ({ appPath, appPlatform, appIsManual, buildApp }) => {
     else if (appPlatform === 'Android') {
       if (!appIsManual) {
         scripOpenFile = `${appPath}/android/app/build/outputs/bundle/release/app-release.aab`;
-        scriptBuild = `cd ${appPath} && npx yarn install && cd android && ./gradlew clean && ./gradlew bundleRelease`;
+        scriptBuild = `cd ${appPath} && npx yarn install && watchman watch-del-all && watchman shutdown-server && cd android && ./gradlew --stop && ./gradlew clean && ./gradlew bundleRelease`;
       }
       else {
         scripOpenFile = `${appPath}/android/app/build/outputs/apk/release/app-release.apk`;
-        scriptBuild = `cd ${appPath} && npx yarn install && cd android && ./gradlew clean && ./gradlew installRelease`;
+        scriptBuild = `cd ${appPath} && npx yarn install && watchman watch-del-all && watchman shutdown-server && cd android && ./gradlew --stop && ./gradlew clean && ./gradlew installRelease`;
       }
     }
 
